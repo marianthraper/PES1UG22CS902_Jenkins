17a6b97053bed425ad35fd7246c5c4f5d34e5b17
marianthraper <113358233+marianthraper@users.noreply.github.com>
2025-03-13 15:24:23 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(void) {
+  cout << "PES1UG22CS902: Maria George Anthraper \n" ;
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
